82484927e0b2a33c3836057de598dbbd93a6c68f
Charles L <charles.lobo@gmail.com>
2024-12-31 14:28:55 +1100

    feat: add log parser

--- logparser.go
+++ logparser.go
@@ -6,6 +6,8 @@ import (
 	"path/filepath"
 	"regexp"
 	"strings"
+	"time"
+	"unicode"
 )
 
 type Log struct {
@@ -14,7 +16,11 @@ type Log struct {
 }
 
 type LogLine struct {
-	raw string
+	Level *string    `json:"level"`
+	On    *time.Time `json:"on"`
+	Src   *string    `json:"src"`
+	Msg   string     `json:msg""`
+	Raw   string     `json:"raw"`
 }
 
 type LogTransform struct {
@@ -45,10 +51,197 @@ func ParseLog(logfile string, transformers []LogTransform) (*Log, error) {
 		lines: []LogLine{},
 	}
 	for _, line := range lines {
-		log.lines = append(log.lines, LogLine{raw: line})
+		log.lines = append(log.lines, parseLogLine(line))
 	}
 
 	return &log, transformerError
+}
+
+var splitRx *regexp.Regexp = regexp.MustCompile(`\s`)
+
+func parseLogLine(line string) LogLine {
+	ll := LogLine{Raw: line}
+
+	tokens := splitRx.Split(line, 10)
+
+	src_count := 0
+	eaten := 0
+
+	for len(tokens) > 0 {
+
+		if len(tokens[0]) == 0 {
+			tokens = tokens[1:]
+			eaten++
+			continue
+
+		}
+
+		if len(tokens) > 0 &&
+			len(tokens[0]) == 1 &&
+			!unicode.IsLetter(rune(tokens[0][0])) && !unicode.IsNumber(rune(tokens[0][0])) {
+
+			tokens = tokens[1:]
+			eaten += 2
+			continue
+		}
+
+		if ll.On == nil {
+			var v *time.Time
+			var eaten_ int
+			v, tokens, eaten_ = popDatetime(tokens)
+			eaten += eaten_
+			ll.On = v
+			if ll.On != nil {
+				continue
+			}
+		}
+
+		if ll.Level == nil {
+			var v *string
+			var eaten_ int
+			v, tokens, eaten_ = popLevel(tokens)
+			eaten += eaten_
+			ll.Level = v
+			if ll.Level != nil {
+				continue
+			}
+		}
+
+		if src_count < 3 {
+			var src *string
+			var eaten_ int
+			src, tokens, eaten_ = popSource(tokens)
+			eaten += eaten_
+			if src != nil {
+				src_count++
+				if ll.Src == nil {
+					ll.Src = src
+				} else {
+					s := (*ll.Src) + " " + *src
+					ll.Src = &s
+				}
+				continue
+			}
+		}
+
+		ll.Msg = line[eaten:]
+		return ll
+
+	}
+
+	return ll
+}
+
+func popSource(tokens []string) (*string, []string, int) {
+	if len(tokens) == 0 {
+		return nil, tokens, 0
+	}
+	if isSource(tokens[0]) {
+		return &tokens[0], tokens[1:], len(tokens[0]) + 1
+	}
+	return nil, tokens, 0
+}
+
+var numRx *regexp.Regexp = regexp.MustCompile(`^\d+$`)
+var ipRx *regexp.Regexp = regexp.MustCompile(`^\d{1,3}(\.\d{1,3}){3}$`)
+
+func isSource(token string) bool {
+	// Check if it's an IP address
+	if ipRx.MatchString(token) {
+		return true
+	}
+
+	// Check if it's a file or class name (e.g., "file.py" or "myModule")
+	exts := []string{
+		".py", ".java", ".js",
+	}
+	for _, ext := range exts {
+		if strings.HasSuffix(token, ext) {
+			return true
+		}
+	}
+
+	// Check for thread-like or module-like patterns (e.g., [main])
+	if strings.HasPrefix(token, "[") && strings.HasSuffix(token, "]") {
+		return true
+	}
+
+	// Check if it's just a number
+	if numRx.MatchString(token) {
+		return true
+	}
+
+	return false
+}
+
+func popLevel(tokens []string) (*string, []string, int) {
+	levels := []string{"ERROR", "WARN", "DEBUG", "INFO", "TRACE", "INF", "ERR"}
+	if len(tokens) == 0 {
+		return nil, tokens, 0
+	}
+	for _, v := range levels {
+		if v == tokens[0] {
+			return &tokens[0], tokens[1:], len(tokens[0]) + 1
+		}
+	}
+	return nil, tokens, 0
+}
+
+var cleanRx *regexp.Regexp = regexp.MustCompile("[^0-9:]")
+
+func popDatetime(tokens []string) (*time.Time, []string, int) {
+
+	formats := []struct {
+		format     string
+		tokenCount int
+	}{
+		// W3C format: %Y-%m-%d %H:%M:%S
+		{"2006-01-02 15:04:05", 2},
+		// Standard golang formats
+		{"2006-01-02T15:04:05Z0700", 1},
+		{time.RFC3339, 1},
+		{time.RFC1123Z, 6},
+		{time.RFC1123, 6},
+		{time.RFC850, 4},
+		{time.RFC822Z, 5},
+		{time.RFC822, 5},
+		{time.RubyDate, 6},
+		{time.UnixDate, 6},
+		{time.ANSIC, 5},
+		// Common format 1: %d/%b/%Y %H:%M:%S
+		{"02/Jan/2006 15:04:05", 2},
+		// Less common format: %d/%b/%Y:%H:%M:%S
+		{"02/Jan/2006:15:04:05", 1},
+		// IIS format: %m/%d/%Y, %H:%M:%S
+		{"01/02/2006, 15:04:05", 2},
+		// Common format 2: %d %b %Y %H:%M:%S
+		{"02 Jan 2006 15:04:05", 4},
+		// Common format 3: %Y %b %d %H:%M:%S
+		{"2006 Jan 02 15:04:05", 4},
+		// ISO 8601 format: %Y-%m-%d %H:%M:%S
+		{"2006-01-02 15:04:05", 2},
+		// RFC 3164 format: %b %d %H:%M:%S (default current year)
+		{"Jan 02 15:04:05", 3},
+		// Db2 format: %Y-%m-%d-%H.%M.%S
+		{"2006-01-02-15.04.05Z07:00", 1},
+		{"2006-01-02-15.04.05Z0700", 1},
+	}
+
+	for _, f := range formats {
+
+		if len(tokens) > f.tokenCount {
+			p := strings.Join(tokens[:f.tokenCount], " ")
+			t, err := time.Parse(f.format, p)
+			if err == nil {
+				if t.Year() < 1900 {
+					t = time.Date(time.Now().Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), t.Nanosecond(), t.Location())
+				}
+				return &t, tokens[f.tokenCount:], len(p) + 1
+			}
+		}
+	}
+
+	return nil, tokens, 0
 }
 
 func applyTransformers(transformers []LogTransform, name string, lines []string) ([]string, error) {
--- logparser_test.go
+++ logparser_test.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"strings"
 	"testing"
+	"time"
 )
 
 func TestLogParser(t *testing.T) {
@@ -34,7 +37,65 @@ func TestLogParser(t *testing.T) {
 	} else {
 		fmt.Println(log.name)
 		for _, l := range log.lines {
-			fmt.Println(l.raw)
+			json, _ := json.MarshalIndent(l, "", "  ")
+			fmt.Println(string(json))
 		}
 	}
+}
+
+func TestDateParser(testing *testing.T) {
+	tests := []string{
+		"2022-04-17 11:25:12.345 This is a test",
+		"17/Apr/2022 11:25:12.345 Another test",
+		"17/Apr/2022:11:25:12.345 Some other test",
+		"04/17/2022, 11:25:12.345 More test",
+		"17 Apr 2022 11:25:12.345 Yet another test",
+		"2022 Apr 17 11:25:12.345 Example again",
+		"2022-04-17 11:25:12.345 ISO 8601 format",
+	}
+
+	std := time.Date(2022, 04, 17, 11, 25, 12, 345*1e6, time.UTC)
+
+	for _, line := range tests {
+		tokens := strings.Fields(line)
+		t, _, _ := popDatetime(tokens)
+		if t == nil {
+			testing.Errorf("Failed test \"%s\"", line)
+		} else if !t.Equal(std) {
+			testing.Errorf("Date incorrect: \"%s\" => %s", line, t.Format(time.RFC3339Nano))
+		}
+	}
+
+	std = time.Date(2022, 04, 17, 11, 25, 12, 345*1e6, time.FixedZone("Custom", -10*60*60))
+	line := "2022-04-17T11:25:12.345-10:00 RFC 3339 format"
+	tokens := strings.Fields(line)
+	t, _, _ := popDatetime(tokens)
+	if t == nil || !t.Equal(std) {
+		testing.Errorf("Failed test \"%s\"", line)
+	}
+
+	std = time.Date(2022, 04, 17, 11, 25, 12, 345*1e6, time.FixedZone("Custom", 10*60*60))
+	line = "2022-04-17T11:25:12.345+1000 RFC 3339 format"
+	tokens = strings.Fields(line)
+	t, _, _ = popDatetime(tokens)
+	if t == nil || !t.Equal(std) {
+		testing.Errorf("Failed test \"%s\" => %s", line, t.Format(time.RFC3339Nano))
+	}
+
+	std = time.Date(time.Now().Year(), 04, 17, 11, 25, 12, 0, time.UTC)
+	line = "Apr 17 11:25:12 Default year"
+	tokens = strings.Fields(line)
+	t, _, _ = popDatetime(tokens)
+	if t == nil || !t.Equal(std) {
+		testing.Errorf("Failed test \"%s\" => %s", line, t.Format(time.RFC3339Nano))
+	}
+
+	std = time.Date(2022, 05, 17, 11, 25, 12, 114*1e6, time.FixedZone("Custom", -3*60*60))
+	line = "2022-05-17-11.25.12.114000-0300 Db2 format"
+	tokens = strings.Fields(line)
+	t, _, _ = popDatetime(tokens)
+	if t == nil || !t.Equal(std) {
+		testing.Errorf("Failed test \"%s\" => %s", line, t.Format(time.RFC3339Nano))
+	}
+
 }
